679bccae4d16b04fd2276b5a625f1f2f55ab2d72
dbordson <bozzod@gmail.com>
2014-06-01 17:06:58 -0500

    static files fix attempt

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -10,3 +10,8 @@ urlpatterns = patterns('',
     url(r'^stockhist/', include('stockhist.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
