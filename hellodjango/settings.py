88a647943237164c0aeaff3c1b6337e1413e535d
dbordson <bozzod@gmail.com>
2014-06-01 17:26:39 -0500

    re-add  static files functionality

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -102,6 +102,6 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
