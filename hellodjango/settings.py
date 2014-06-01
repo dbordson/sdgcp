a06db5a498e7934c2ef08721c3819e9b1297a074
dbordson <bozzod@gmail.com>
2014-06-01 16:45:23 -0500

    test db config fix

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -99,7 +99,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-BASE_DIR = os.path.dirname(os.abspath.dirname(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
