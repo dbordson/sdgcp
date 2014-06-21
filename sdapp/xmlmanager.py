17672aa6fd3b5ccaf7764016cb603f76017f58f9
dbordson <bozzod@gmail.com>
2014-06-21 12:36:05 -0500

    update for local testing

--- sdapp/xmlmanager.py
+++ sdapp/xmlmanager.py
@@ -1,18 +1,16 @@
 def filemapper(CIK, form):
-	import os
-	xmldirectory = []
-	cwd = os.getcwd()
-	
-	if form == '4/A':
-		form = '4A'
+    import os
+    xmldirectory = []
+    cwd = os.getcwd()
 
-	for root, dirs, files in os.walk (cwd + "/AutomatedFTP/storage/" + str(CIK) + "/" + str(form) + "/"):
-		for file in files:
-			if file.endswith('.xml'):
-#				if os.path.join(root, file).find("0001179110") == -1:
-#					print os.path.join(root, file)
-				xmldirectory.append(os.path.join(root, file))
-	return xmldirectory
+    if form == '4/A':
+        form = '4A'
 
-
-
+    for root, dirs, files in os.walk(cwd + "/AutomatedFTP/storage/" +
+                                     str(CIK) + "/" + str(form) + "/"):
+        for file in files:
+            if file.endswith('.xml'):
+#               if os.path.join(root, file).find("0001179110") == -1:
+#                   print os.path.join(root, file)
+                xmldirectory.append(os.path.join(root, file))
+    return xmldirectory
