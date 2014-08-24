5d153984fd933504c3bc1fa182757c92427fe75a
dbordson <bozzod@gmail.com>
2014-08-24 11:48:58 -0500

    added aggholdingtype display as "totals" for the holding table -- aggholdingtype will be primarily used, however, for relevance testing

--- sdapp/views.py
+++ sdapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from sdapp.models import CompanyStockHist, ClosePrice, IssuerCIK,\
-    Form345Entry, Affiliation, Holding, HoldingType
+    Form345Entry, Affiliation, Holding, HoldingType, AggHoldingType
 import datetime
 
 
@@ -55,7 +55,7 @@ def holdingdetail(request, ticker_sym):
 def holdingtable(request, ticker_sym):
     issuer = \
         IssuerCIK.objects.filter(companystockhist__ticker_sym=ticker_sym)[0]
-    lookbackdays = 548
+    lookbackdays = 365 * 100
     startdate = datetime.date.today() - datetime.timedelta(lookbackdays)
     affiliationset = Affiliation.objects.filter(issuer=issuer)\
         .filter(most_recent_filing__gte=startdate)
@@ -66,6 +66,9 @@ def holdingtable(request, ticker_sym):
     stockholdingtitles = list(set(stockholdingset
                               .values_list('security_title', flat=True)
                               .distinct()))
+    totalset = AggHoldingType.objects.filter(issuer=issuer)\
+        .exclude(units_held=None)
+    stocktotals = totalset.filter(deriv_or_nonderiv='N')
     stockholdinglists = []
     for title in stockholdingtitles:
         stockholdinglist = []
@@ -74,6 +77,9 @@ def holdingtable(request, ticker_sym):
             .order_by('-units_held')[:5]
         # print titleholdings.values_list('owner', flat=True)
         stockholdinglist.append(titleholdings)
+
+        total = stocktotals.filter(security_title=title)[0]
+        stockholdinglist.append(total)
         stockholdinglists.append(stockholdinglist)
         # print title
         # print stockholdinglist
@@ -82,6 +88,7 @@ def holdingtable(request, ticker_sym):
     derivholdingtitles = list(set(derivholdingset
                               .values_list('security_title', flat=True)
                               .distinct()))
+    derivtotals = totalset.filter(deriv_or_nonderiv='D')
     derivholdinglists = []
     for title in derivholdingtitles:
         derivholdinglist = []
@@ -89,6 +96,8 @@ def holdingtable(request, ticker_sym):
         titleholdings = derivholdingset.filter(security_title=title)\
             .order_by('-units_held')[:5]
         derivholdinglist.append(titleholdings)
+        total = derivtotals.filter(security_title=title)[0]
+        derivholdinglist.append(total)
         derivholdinglists.append(derivholdinglist)
     # print stockholdinglists
     return render_to_response('sdapp/holdingtable.html',
