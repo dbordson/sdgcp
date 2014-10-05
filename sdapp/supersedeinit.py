4385f05215b39cf82f84405b6df1a520dbfb8f15
dbordson <bozzod@gmail.com>
2014-10-05 17:07:42 -0500

    added superseded date initialization script

---
+++ sdapp/supersedeinit.py
@@ -0,0 +1,32 @@
+from sdapp.models import Form345Entry
+# from django.db import connection
+# import datetime
+
+
+# all_entries, untagged_entries
+
+def superseded_initialize():
+    all_entries = Form345Entry.objects\
+        .exclude(transaction_date=None)
+    untagged_entries = all_entries.filter(supersededdt=None)
+    looplength = len(untagged_entries)
+    counter = 0
+    for untagged_entry in untagged_entries:
+        counter = counter + 1
+        print "%s of %s" % (counter, looplength)
+        filtered_entries = all_entries\
+            .filter(issuer_cik_num=untagged_entry.issuer_cik_num)\
+            .filter(reporting_owner_cik_num=untagged_entry
+                    .reporting_owner_cik_num)\
+            .filter(security_title=untagged_entry.security_title)\
+            .filter(filedatetime__gt=untagged_entry.filedatetime)\
+            .order_by('filedatetime')
+        if filtered_entries.exists():
+            untagged_entry.supersededdt = filtered_entries[0].filedatetime
+            untagged_entry.save()
+    return
+
+# How do you best remove the entries recently tagged as superseded from the
+# query set?
+
+superseded_initialize()
