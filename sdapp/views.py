488c6b689108e96ef74f38f9512cb0909c31783e
dbordson <bozzod@gmail.com>
2014-12-17 15:39:14 -0600

    tweaked views file

--- sdapp/views.py
+++ sdapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from sdapp.models import Security, ClosePrice, SecurityPriceHist,\
+from sdapp.models import Security, \
     Form345Entry, PersonHoldingView, SecurityView
 # import datetime
 
