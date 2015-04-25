17e6c75ee753726b0d768ddbaa2f01d42745a822
dbordson <bozzod@gmail.com>
2015-04-24 20:22:39 -0500

    updated admin page so that reporting person names can readily be searched and updated -- these are pretty inconstently formatted at the source and there are lots of reporting persons (17k in our data set), so we will need an automated means of standardizing the names, but if we notice we missed some with the automated script, I would like it to be straightforward to fix them.

--- sdapp/admin.py
+++ sdapp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import IssuerCIK
+from .models import IssuerCIK, ReportingPerson
+
+
+class ReportingPersonAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['person_name']}),
+        (None,              {'fields': ['reporting_owner_cik_num']})
+    ]
+    search_fields = ['person_name']
 
 admin.site.register(IssuerCIK)
+admin.site.register(ReportingPerson, ReportingPersonAdmin)
