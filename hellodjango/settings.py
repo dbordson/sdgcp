91c31cf7151f8a8db5cb4de7516390fe58e910bb
dbordson <bozzod@gmail.com>
2015-05-16 22:03:59 -0500

    Update settings.py
    Tweak to static files for heroku

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -116,11 +116,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-if DEBUG:
-    # MEDIA_URL = '/media/'
-    STATIC_ROOT =\
-        os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
-    # MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
-    STATICFILES_DIRS = (
-        os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
-    )
+# if DEBUG:
+#     MEDIA_URL = '/media/'
+#     STATIC_ROOT =\
+#         os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
+#     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+#     STATICFILES_DIRS = (
+#         os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+#     )
