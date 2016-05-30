1206859f8954e394853d9da6ea978f1ee4c9be6a
dbordson <bozzod@gmail.com>
2016-05-29 22:41:26 -0500

    another update because GAE does not appear to support direct FTP access.

--- sdapp/bin/bulkupdate.py
+++ sdapp/bin/bulkupdate.py
@@ -1,8 +1,8 @@
 # TO RUN THIS IN GCP:
 # First run "from sdapp.bin import initialdownload" in a local shell.
-# because GCP can't run "initialdownload.py", because it does not appear to
-# support FTP access.
-from sdapp.bin import formscraper, formparser, populateintermediate,\
+# because GCP can't run "initialdownload.py" or formscraper.py, because it
+# does not appear to support FTP access.
+from sdapp.bin import formparser, populateintermediate,\
     grabmarketdata,\
     populateformadjustments, supersedeinit, adjsharesremaining
 
